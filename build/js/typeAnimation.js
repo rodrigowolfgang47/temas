0f05b4a98a6e351197998e4367bd757b84c83785
Rodrigo Costa <rodrigocosta47@outlook.com>
2023-02-10 14:35:20 -0300

    Corrigo o erro das pedras voando

---
+++ build/js/typeAnimation.js
@@ -0,0 +1,8 @@
+var options = {
+  strings: ['Olá, me chamo Rodrigo', 'Sou desenvolvedor. . .', 'Python, Php, Javacript'  ],
+  typeSpeed: 30,
+  backSpeed: 20,
+  loop:true
+};
+
+var typed = new Typed('.sobre-mim h1', options);
